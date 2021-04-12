aac2aa1b0dfb56f48d947fc97c0cbe38f5e0cc82
abhishekgedelacode <gabhishekji143@gmail.com>
2021-04-12 22:44:26 +0530

    created about services contacts page and edited the about page with required content

--- land/views.py
+++ land/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'land/index.html')
+
+
+def about(request):
+    return render(request, 'land/about.html')
+
+
+def services(request):
+    return render(request, 'land/services.html')
+
+
+def contacts(request):
+    return render(request, 'land/contacts.html')
